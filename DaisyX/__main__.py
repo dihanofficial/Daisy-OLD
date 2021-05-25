5ccd9bb09716ae4c2f1414efac3cb4b327cd4199
Dihan Official <84424959+dihanrandila1@users.noreply.github.com>
2021-05-25 21:51:14 +0530

    Update __main__.py

--- DaisyX/__main__.py
+++ DaisyX/__main__.py
@@ -101,7 +101,7 @@ HELP_STRINGS = f"""
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
 You can donate to the original writer's of the Base code,
-Support them  [Dihan](t.me/dihanrandila),[Jason](t.me/dihanrandila),"""
+Support them  [Dihan](t.me/dihanrandila),[Dihan](t.me/dihanrandila),"""
 
 IMPORTED = {}
 MIGRATEABLE = []
